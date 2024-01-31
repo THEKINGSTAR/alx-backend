18d623bef7b3158e104bdf0b36e36f0040851065
THEKINGSTAR <thekingstar5002@gmail.com>
2024-01-31 23:38:07 +0200

    testing 4. MRU Caching

---
+++ 0x01-caching/4-main.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+""" 4-main """
+MRUCache = __import__('4-mru_cache').MRUCache
+
+my_cache = MRUCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+print(my_cache.get("B"))
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+print(my_cache.get("A"))
+print(my_cache.get("B"))
+print(my_cache.get("C"))
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
+my_cache.put("H", "H")
+my_cache.print_cache()
+my_cache.put("I", "I")
+my_cache.print_cache()
+my_cache.put("J", "J")
+my_cache.print_cache()
+my_cache.put("K", "K")
+my_cache.print_cache()
+
+
+"""
+guillaume@ubuntu:~/0x01$ ./4-main.py
+Current cache:
+A: Hello
+B: World
+C: Holberton
+D: School
+World
+DISCARD: B
+Current cache:
+A: Hello
+C: Holberton
+D: School
+E: Battery
+Current cache:
+A: Hello
+C: Street
+D: School
+E: Battery
+Hello
+None
+Street
+DISCARD: C
+Current cache:
+A: Hello
+D: School
+E: Battery
+F: Mission
+DISCARD: F
+Current cache:
+A: Hello
+D: School
+E: Battery
+G: San Francisco
+DISCARD: G
+Current cache:
+A: Hello
+D: School
+E: Battery
+H: H
+DISCARD: H
+Current cache:
+A: Hello
+D: School
+E: Battery
+I: I
+DISCARD: I
+Current cache:
+A: Hello
+D: School
+E: Battery
+J: J
+DISCARD: J
+Current cache:
+A: Hello
+D: School
+E: Battery
+K: K
+guillaume@ubuntu:~/0x01$ 
+"""
